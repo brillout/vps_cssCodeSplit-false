44944b028d4e59475ce48b564420b35df966f1be
Romuald Brillout <git@brillout.com>
2023-02-12 00:49:06 +0100

    disable code split

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,8 @@ import react from '@vitejs/plugin-react'
 import ssr from 'vite-plugin-ssr/plugin'
 
 export default {
-  plugins: [react(), ssr()]
+  plugins: [react(), ssr()],
+  build: {
+    cssCodeSplit: false
+  }
 }
